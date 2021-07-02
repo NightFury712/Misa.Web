0ad41569515b9736e4ea85e7927a3ba4cbd6eafb
DANG HOANG HAI <hhdang@rd.local>
2021-07-02 13:40:21 +0700

    updated 2/7

--- js/page/employee.js
+++ js/page/employee.js
@@ -28,29 +28,16 @@ function showDialog() {
 // Toggle sitebar in out
 function toggleSitebar() {
     $('.btn-toggle-navbar').click(() => {
-        var classList = $('.toggle-navbar .btn-toggle-navbar .arrow-white').attr('class').split(/\s+/);
         if ($('.navbar').width() == 220) {
-            $('.navbar').width(54);
-            $('.header').width('calc(100% - 55px)')
-            $('.content').width('calc(100% - 87px)')
+            $('.navbar').width(68);
+            $('.header').width('calc(100% - 69px)')
+            $('.content').width('calc(100% - 101px)')
             $('.nav-item-text').hide();
             $('.logo-box .amis-logo').hide();
-            $.each(classList, function(index, item) {
-                if (item == 'left') {
-                    $('.toggle-navbar .btn-toggle-navbar .arrow-white').removeClass(item)
-                    $('.toggle-navbar .btn-toggle-navbar .arrow-white').addClass('right')
-                }
-            })
         } else {
             $('.navbar').width(220)
             $('.header').width('calc(100% - 221px)')
             $('.content').width('calc(100% - 253px)')
-            $.each(classList, function(index, item) {
-                if (item == 'right') {
-                    $('.toggle-navbar .btn-toggle-navbar .arrow-white').removeClass(item)
-                    $('.toggle-navbar .btn-toggle-navbar .arrow-white').addClass('left')
-                }
-            })
             setTimeout(function() {
                 $('.nav-item-text').show();
                 $('.logo-box .amis-logo').show();
